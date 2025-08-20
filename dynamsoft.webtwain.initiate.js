1edf2c7f417f8fa0787a7961fe68c60e285c2da5
dabresaad-alt <dabresaad@gmail.com>
2025-08-20 17:18:28 +0300

    Update dynamsoft.webtwain.initiate.js

--- dynamsoft.webtwain.initiate.js
+++ dynamsoft.webtwain.initiate.js
@@ -6392,7 +6392,7 @@ if (!Dynamsoft.Lib) {
 		DynamicDWTMap: {},
 		ProductKey: "",
 		Trial: true,
-		ResourcesPath: "https://https://dabresaad-alt.github.io/dynamsoft-webtwain-resource/",
+		ResourcesPath: "https://dabresaad-alt.github.io/dynamsoft-webtwain-resource",
 		Debug: false,
 		IfUpdateService: false,
 		AutoLoad: true,
